fb4415165325ca897a6eb56ce68d143e950cc244
Marvin Steven Jimenez Rodriguez <stev.199279@gmail.com>
2022-11-29 23:59:17 -0600

    Add project files.

---
+++ proyecto/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProyectoProgra6Entities db = new ProyectoProgra6Entities();
+        public ActionResult Index()
+        {
+            try
+            {
+                if (Session["usuario"] != null)
+                {
+                    var idUsuario = Session["usuario"];
+                    var clientelog = db.sp_Seleccionar_Cliente((int)idUsuario).ToList();
+                    Session["clientelog"] = clientelog;
+                    ViewBag.clientelog = clientelog;
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewData["Mensaje"] = "Ocurrio un error : " + ex.Message;
+                return View();
+            }
+
+        }
+        public ActionResult LogOut()
+        {
+            Session["usuario"] = null;
+            Session["clientelog"] = null;
+            return RedirectToAction("Login", "Login");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
