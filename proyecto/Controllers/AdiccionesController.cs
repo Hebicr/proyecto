c4acb95a3359fc440519d935dde527dc9e432bc2
JorgeCQ75 <quiroschacon10@outlook.com>
2022-12-01 09:02:31 -0600

    2.3

---
+++ proyecto/Controllers/AdiccionesController.cs
@@ -0,0 +1,88 @@
+﻿using proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto.Controllers
+{
+    public class AdiccionesController : Controller
+    {
+        // GET: Adicciones
+        ProyectoProgra6Entities db = new ProyectoProgra6Entities();
+        //OBTENER UNA LISTA DE LAS ADICCIONES DE POLIZAS DISPONIBLES
+        public ActionResult Index()
+        {
+            var adicciones = db.sp_getAdicciones();
+            return View(adicciones);
+        }
+        //VISTA PARA CREAR UNA NUEVA COBERTURA
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //EL HTTPPOST PARA ENVIAR LA INFORMACION CON LOS QUE SE VAN A CREAR LA NUEVA ADICCION
+        [HttpPost]
+        public ActionResult Create(FormCollection myAdiccion)
+        {
+            try
+            {
+                Adicione adiccion = new Adicione();
+                adiccion.Nombre = myAdiccion["Nombre"];
+                adiccion.Codigo = myAdiccion["Codigo"];
+                db.sp_AgregarAdiccion(adiccion.Nombre, adiccion.Codigo);
+
+                return RedirectToAction("Index", "Adicciones");
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+        //LA VISTA DEL EDITAR PARA CARGAR LOS DATOS
+        public ActionResult Editar(int idAdiccion)
+        {
+            Adicione Adicion  = (from c in db.Adiciones where idAdiccion == c.idAdiccion select c).First();
+            return View(Adicion);
+
+            //var adiccion = db.sp_getAdiccionEditar(idAdiccion);
+            //return View(adiccion);
+        }
+
+        // HTTPPOST PARA ENVIAR LA INFORMACION DEL EDITAR 
+        [HttpPost]
+        public ActionResult Editar(int idAdiccion, FormCollection myAdiccion)
+        {
+            try
+            {
+                Adicione adiccion = new Adicione();
+                adiccion.idAdiccion = Convert.ToInt32(myAdiccion["idCoberturaPoliza"]);
+                adiccion.Nombre = myAdiccion["Nombre"];
+                adiccion.Codigo = myAdiccion["Codigo"];
+                db.sp_ModificarAdiccion(adiccion.idAdiccion, adiccion.Nombre, adiccion.Codigo);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //ELIMINAR ADICCIONES DE POLIZAS
+        public ActionResult EliminarAdiccion(int idAdiccion)
+        {
+            try
+            {
+                db.sp_eliminarAdiccion(idAdiccion);
+                return RedirectToAction("Index", "Adicciones");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ proyecto/Controllers/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+
+        ProyectoProgra6Entities db = new ProyectoProgra6Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Adicciones()
+        {
+            List<Adicione> adiccionList = db.Adiciones.ToList();
+            ViewBag.adiccionList = new SelectList(adiccionList, "idAdiccion", "Nombre");
+
+
+
+            return View();
+        }
+    }
+}
